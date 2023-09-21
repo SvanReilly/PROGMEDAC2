9be0e35e5b8a94cb6365daee27b6bbe90d2e83fb
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2023-09-21 21:17:40 +0200

    Interfaces1

--- DesarrolloDeInterfaces/interfaz1/JFrame1.java
+++ DesarrolloDeInterfaces/interfaz1/JFrame1.java
@@ -6,12 +6,22 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JProgressBar;
+import javax.swing.JTextPane;
+import javax.swing.JPasswordField;
+import javax.swing.JSeparator;
 
 @SuppressWarnings("unused")
 public class JFrame1 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -34,11 +44,44 @@ public class JFrame1 extends JFrame {
 	 */
 	public JFrame1() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 499, 358);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
+		setBounds(100, 100, 826, 567);
+		getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel);
+		panel.setLayout(new GridLayout(0, 5, 0, 0));
+		
+		JPanel panel_5 = new JPanel();
+		panel.add(panel_5);
+		
+		JButton btnNewButton = new JButton("Useless button");
+		panel_5.add(btnNewButton);
+		
+		JSeparator separator = new JSeparator();
+		panel_5.add(separator);
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("Check if you dumb");
+		panel_5.add(chckbxNewCheckBox);
+		
+		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("Check if you asshole");
+		chckbxNewCheckBox_1.setVerticalAlignment(SwingConstants.BOTTOM);
+		panel_5.add(chckbxNewCheckBox_1);
+		
+		JTextArea txtrTellMeAbout = new JTextArea();
+		panel_5.add(txtrTellMeAbout);
+		txtrTellMeAbout.setText("Write down here");
+		
+		JTextPane txtpnYouDefinitelyDumbass = new JTextPane();
+		txtpnYouDefinitelyDumbass.setText("you definitely dumbass at:");
+		panel_5.add(txtpnYouDefinitelyDumbass);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setValue(94);
+		progressBar.setStringPainted(true);
+		panel_5.add(progressBar);
+		
+		JPanel panel_7 = new JPanel();
+		panel.add(panel_7);
 	}
 
 }
