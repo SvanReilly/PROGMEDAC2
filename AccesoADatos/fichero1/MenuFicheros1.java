0444a65186bee72c5bbea54bfb9ebc023a68d937
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2023-09-25 18:09:35 +0200

    Update MenuFicheros1.java

--- AccesoADatos/fichero1/MenuFicheros1.java
+++ AccesoADatos/fichero1/MenuFicheros1.java
@@ -19,13 +19,19 @@ public class MenuFicheros1 {
             System.out.println("Bienvenido al Gestor de Ficheros Masónicos.");
             
             System.out.println(
-            "1.- Crear fichero. "
+              "1.- Crear fichero. "
             + "\n"
             + "2.- Borrar fichero."
             + "\n"
             + "3.- Mover fichero."
             + "\n"
-            + "4.- Salir del programa.");
+            + "4.- Crear directorio."
+            + "\n"
+            + "5.- Leer fichero."
+            + "\n"
+            + "6.- Escribir fichero."
+            + "\n"
+            + "7.- Salir del programa.");
 
 			System.out.println("\n" + "¿Qué desea hacer? Inserte un numero del 1-4, por favor:");
 			String value = sc.next();
@@ -76,12 +82,21 @@ public class MenuFicheros1 {
 				}
 
 				break;
+				
 			case "4":
 				
 				System.out.println(""
 						+ "Hasta pronto.");
 				menu = false;
 				break;
+				
+			case "7":
+				
+				System.out.println(""
+						+ "Hasta pronto.");
+				menu = false;
+				break;
+
 			default:
 
 				System.out.println("\n\n"
