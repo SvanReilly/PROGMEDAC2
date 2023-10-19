030dd3a5946c0d3fb94b3bfe1da54f69946f13d6
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2023-10-19 20:26:49 +0200

    19/10/2023
    Programación Multimedia:
    
    //

--- ProgramacionMultimediaProyectos/Practica5/app/src/main/java/com/example/practica5/MainActivity.java
+++ ProgramacionMultimediaProyectos/Practica5/app/src/main/java/com/example/practica5/MainActivity.java
@@ -5,12 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    String correo,pass;
+    private String correo,pass;
     private Button buttonContinuar;
     private Switch recordarUsuario;
     private TextView hiddenText;
@@ -24,38 +23,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonContinuar = findViewById(R.id.buttonContinuar);
         hiddenText = findViewById(R.id.resultText);
-        buttonContinuar.setOnClickListener(this);
         correoIns=findViewById(R.id.cajaTextoCorreo);
         passIns=findViewById(R.id.cajaTextoPass);
         recordarUsuario=findViewById(R.id.switchRecordar);
+
+        buttonContinuar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        correo= correoIns.getText().toString();
-        pass=passIns.getText().toString();
-        if (correo.equals("correo@correo.com") && pass.equals("123") ){
-            hiddenText.setVisibility(View.VISIBLE);
-            hiddenText.setTextColor(getColor(R.color.green));
-            hiddenText.setText("Usuario y contraseña correctos.");
-        } else if (correo.equals("correo@correo.com") && pass.equals("123") && recordarUsuario.isChecked()){
-            hiddenText.setVisibility(View.VISIBLE);
-            hiddenText.setTextColor(getColor(R.color.green));
-            hiddenText.setText("Usuario y contraseña correctos. " +
-                    "\nSe almacenó la información para próximos accesos.");
-        } else if (!correo.equals("correo@correo.com") && pass.equals("123")){
-            hiddenText.setVisibility(View.VISIBLE);
-            hiddenText.setTextColor(getColor(R.color.orange));
-            hiddenText.setText("Usuario incorrecto.");
-        } else if (correo.equals("correo@correo.com") && !pass.equals("123")) {
-            hiddenText.setVisibility(View.VISIBLE);
-            hiddenText.setTextColor(getColor(R.color.orange));
-            hiddenText.setText("Contraseña incorrecta.");
-        } else {
-            hiddenText.setVisibility(View.VISIBLE);
-            hiddenText.setTextColor(getColor(R.color.red));
-            hiddenText.setText("Usuario y contraseña incorrectos.");
-        }
+        correo = correoIns.getText().toString();
+        pass = passIns.getText().toString();
+        if (view.getId() == R.id.buttonContinuar) {
+            if (correo.equals("correo@correo.com") && pass.equals("123")) {
+                hiddenText.setVisibility(View.VISIBLE);
+                hiddenText.setTextColor(getColor(R.color.green));
+                hiddenText.setText("Usuario y contraseña correctos.");
+                if (correo.equals("correo@correo.com") && pass.equals("123") && recordarUsuario.isChecked()) {
+                    hiddenText.setText(hiddenText.getText() +
+                            "\n" + "Se almacenó la información para próximos accesos.");
+                }
+                } else {
+                    hiddenText.setVisibility(View.VISIBLE);
+                    hiddenText.setTextColor(getColor(R.color.red));
+                    hiddenText.setText("Usuario y/o contraseña incorrectos.");
+                }
 
+            }
+        }
     }
-}
