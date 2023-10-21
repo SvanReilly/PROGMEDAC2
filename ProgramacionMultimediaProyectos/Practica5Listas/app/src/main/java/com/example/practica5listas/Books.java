253ccc06e31dbaff5fb2949148ef3bb0b6dd31a9
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2023-10-21 05:19:31 +0200

    21/10/2023
    Programación Multimedia:
    
    //Práctica 5, Botones y Listas (definitivamente completo)

--- ProgramacionMultimediaProyectos/Practica5Listas/app/src/main/java/com/example/practica5listas/Books.java
+++ ProgramacionMultimediaProyectos/Practica5Listas/app/src/main/java/com/example/practica5listas/Books.java
@@ -5,7 +5,6 @@ public class Books{
     private String frontCover;
     private String releaseDate;
     private String ISBNCode;
-
     public Books(String title, String author, String frontCover, String releaseDate, String ISBNCode) {
         this.title = title;
         this.author = author;
@@ -13,44 +12,33 @@ public class Books{
         this.releaseDate = releaseDate;
         this.ISBNCode = ISBNCode;
     }
-
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getAuthor() {
         return author;
     }
-
     public void setAuthor(String author) {
         this.author = author;
     }
-
     public String getFrontCover() {
         return frontCover;
     }
-
     public void setFrontCover(String frontCover) {
         this.frontCover = frontCover;
     }
-
     public String getReleaseDate() {
         return releaseDate;
     }
-
     public void setReleaseDate(String releaseDate) {
         this.releaseDate = releaseDate;
     }
-
     public String getISBNCode() {
         return ISBNCode;
     }
-
     public void setISBNCode(String ISBNCode) {
         this.ISBNCode = ISBNCode;
     }
--- ProgramacionMultimediaProyectos/Practica5Listas/app/src/main/java/com/example/practica5listas/MainActivity.java
+++ ProgramacionMultimediaProyectos/Practica5Listas/app/src/main/java/com/example/practica5listas/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.practica5listas;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,15 +16,25 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recView);
 
         Books[] book = new Books[4];
-        book[0] = new Books("The Silmarillion", "Tolkien, J. R. R.", "silmarillioncover", "2022-11-08", "0063280779" );
-        book[1] = new Books("The Hobbit", "Tolkien, J. R. R.", "hobbitcover", "2012-09-18", "0547928246" );
-        book[2] = new Books("The Lord of the Rings", "J.R.R. Tolkien, J. R. R.", "lotrcover", "2021-11-16", "0358653037" );
-        book[3] = new Books("1984", "George Orwell", "cover1984", "1961-01-01", "0451524934" );
+        book[0] = new Books(
+                "The Silmarillion","Tolkien, J. R. R.",
+                "silmarillioncover",
+                "2022-11-08", "0063280779" );
+        book[1] = new Books("The Hobbit", "Tolkien, J. R. R.",
+                "hobbitcover",
+                "2012-09-18", "0547928246" );
+        book[2] = new Books("The Lord of the Rings", "Tolkien, J. R. R.",
+                "lotrcover",
+                "2021-11-16", "0358653037" );
+        book[3] = new Books("1984", "George Orwell",
+                "cover1984",
+                "1961-01-01", "0451524934" );
 
         BookAdapter adapter = new BookAdapter(book);
 
         recyclerView.setHasFixedSize(true);
-        LinearLayoutManager linearLayoutManager= new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        LinearLayoutManager linearLayoutManager= new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
 
         recyclerView.setAdapter(adapter);
