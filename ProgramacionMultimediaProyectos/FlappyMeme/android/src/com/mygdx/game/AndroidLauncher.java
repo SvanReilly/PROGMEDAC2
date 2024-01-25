a06cd4611a7cecb136b1ebbd28cb2a58940ec229
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2024-01-25 18:35:01 +0100

    25/01/2024
    Programación Multimedia:
    
    //FlappyMemeBird (Creada)

--- ProgramacionMultimediaProyectos/FlappyMeme/android/src/com/mygdx/game/AndroidLauncher.java
+++ ProgramacionMultimediaProyectos/FlappyMeme/android/src/com/mygdx/game/AndroidLauncher.java
@@ -1,16 +1,16 @@
-package com.mygdx.flappymeme;
+package com.mygdx.game;
 
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.flappymeme.MyGdxGame;
+import com.mygdx.game.FlappyBirdGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGame(), config);
+		initialize(new FlappyBirdGame(), config);
 	}
 }
--- ProgramacionMultimediaProyectos/FlappyMeme/core/src/com/mygdx/game/FlappyBirdGame.java
+++ ProgramacionMultimediaProyectos/FlappyMeme/core/src/com/mygdx/game/FlappyBirdGame.java
@@ -1,11 +1,11 @@
-package com.mygdx.flappymeme;
+package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class FlappyBirdGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
